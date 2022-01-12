a4cfc1a5261c8358761762e4e55949e1d2398569
EfNik <s.nikolakeas@outlook.com>
2022-01-12 17:01:38 +0200

    test cors

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ app.use(bodyParser.json()); // semd json repsonses
 app.use(logger('dev')); // log request to api using morgan
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ next();
+});
+
 require('dotenv').config();
 
 // Listen
